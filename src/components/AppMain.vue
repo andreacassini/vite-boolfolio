ad582f38a9880215cdb762e4a88f21fd61a31fdd
andreacassini <andrea1316@hotmail.it>
2023-09-03 17:47:34 +0200

    bonus impaginazione

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -10,19 +10,22 @@ export default {
         return {
             baseUrl: 'http://localhost:8000',
             posts: [],
+            currentPage: 1,
+            lastPage: null,
             loading: true
         }
     },
     created() {
-        this.getPosts();
+        this.getPosts(1);
     },
     methods: {
-        getPosts() {
+        getPosts(num_page) {
             this.loading = true;
-            axios.get(`${this.baseUrl}/api/posts`).then((response) => {
-                console.log(response);
+            axios.get(`${this.baseUrl}/api/posts`, { params: { page: num_page } }).then((response) => {
                 if (response.data.success) {
-                    this.posts = response.data.results;
+                    this.posts = response.data.results.data;
+                    this.currentPage = response.data.results.current_page;
+                    this.lastPage = response.data.results.last_page;
                     this.loading = false;
                 }
                 else {
@@ -30,19 +33,60 @@ export default {
                 }
             })
 
-        }
+        },
     }
 }
 </script>
 <template>
     <div>
-        <AppLoader v-if="loading"/>
-        <div v-else class="container">
+        <div class="container">
             <div class="row">
                 <div class="col-12">
                     <h1 class="text-center">Boolfolio</h1>
+                    <AppLoader v-if="loading"/>
+                    <div class="col-4 my-4" v-else v-for="post in posts" :key="post.id">
+                        <div class="card h-100">
+                            <img class="card-img-top img-fluid" :src="`${baseUrl}/storage/${post.cover_image}`" :alt="`${post.title}-image`" v-if="post.cover_image">
+                            <div class="card-body h-50">
+                                <!-- TITLE -->
+                                <h5 class="card-title py-2" v-text="post.title"></h5>
+
+                                <!-- DESCRIPTION -->
+                                <p class="card-text py-2" v-text="post.description"></p>
+
+                                 <!-- TYPE -->
+                                <div class="py-4" v-if="post.type">
+                                    <label class="fw-bold mx-1">Tipologia:</label>
+                                    <span v-text="post.type.name"></span>
+                                </div>
+
+                                <!-- TECHNOLOGIES -->
+                                <div class="py-2" v-if="post.technologies">
+                                    <label class="fw-bold mx-1">Tecnologie:</label>
+                                    <ul>
+                                        <li v-for="technology in post.technologies" v-text="technology.name" class="my-2" :key="technology.id"></li>
+                                    </ul>
+                                </div>
+                            </div>
+                            <div class="card-footer text-center">
+                                <a href="#" class="btn btn-primary">Visualizza Post</a>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
+            <div class="col-12 my-5" v-if="!loading">
+               <nav class="d-flex justify-content-center">
+                   <ul class="pagination">
+                       <li :class="currentPage === 1 ? 'disabled' : ''">
+                           <button class="page-link" @click="getPosts(currentPage - 1)">Precedente</button>
+                       </li>
+                       <li :class="currentPage === lastPage ? 'disabled' : ''">
+                           <button class="page-link" @click="getPosts(currentPage + 1)">Successivo</button>
+                       </li>
+                   </ul>
+               </nav>
+           </div>
         </div>
     </div>
 </template>
