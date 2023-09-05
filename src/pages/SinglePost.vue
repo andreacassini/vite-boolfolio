5c1473bc6de1d39528ab833457165f5b322240c0
andreacassini <andrea1316@hotmail.it>
2023-09-05 19:35:53 +0200

    add singlepost component

---
+++ src/pages/SinglePost.vue
@@ -0,0 +1,35 @@
+<script>
+import axios from 'axios';
+import { store } from '../store';
+
+export default {
+    name: 'SinglePost',
+    data(){
+        return{
+            store,
+            post: null,
+
+        }
+    },
+    created(){
+
+    },
+    methods:{
+        getSinglePost(){
+            this.store.loading = true;
+            axios.get(`${this.store.baseUrl}/api/posts/${this.$route.params.slug}`).then((response)=>{
+
+            });
+        }
+    }
+}
+</script>
+
+<template lang="">
+    <div>
+        
+    </div>
+</template>
+<style lang="">
+    
+</style>
